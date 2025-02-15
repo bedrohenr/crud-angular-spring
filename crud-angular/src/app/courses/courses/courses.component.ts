37ead669b0a91c5e807a643ee1c82beb2ceb0043
Pedro Ferreira <ferfonhp@gmail.com>
2025-02-15 14:26:45 -0300

    usando nova interface

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
+  constructor(){
+    // this.courses = [];
+  }
 
+  courses: Course[] = [];
 }
