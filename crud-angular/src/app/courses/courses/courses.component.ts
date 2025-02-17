08a4109c2bd59935cf29d4033cf49fbfd8c1ef67
Pedro Ferreira <ferfonhp@gmail.com>
2025-02-17 12:25:02 -0300

    adicionado spinner antes do retorno dos dados

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -3,19 +3,20 @@ import { Course } from '../model/course';
 import { AppMaterialModule } from '../../shared/app-material/app-material.module';
 import { CoursesService } from '../services/courses.service';
 import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-courses',
-  imports: [AppMaterialModule],
+  imports: [CommonModule, AppMaterialModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
-  courses: Observable<Course[]>;
+  courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
   constructor(private coursesService: CoursesService){
-    this.courses = this.coursesService.list();
+    this.courses$ = this.coursesService.list();
     // this.courses = [];
     // this.coursesService = new CoursesService();
   }
