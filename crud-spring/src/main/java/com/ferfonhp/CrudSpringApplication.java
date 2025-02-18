61861f7aada0dee93dc165d518d0e18e7098cab3
Pedro Ferreira <ferfonhp@gmail.com>
2025-02-18 14:45:12 -0300

    Mudanças para fins de teste

--- crud-spring/src/main/java/com/ferfonhp/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/ferfonhp/CrudSpringApplication.java
@@ -1,7 +1,12 @@
 package com.ferfonhp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.ferfonhp.model.Course;
+import com.ferfonhp.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -10,4 +15,15 @@ public class CrudSpringApplication {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+			Course c = new Course();
+			c.setName("Angular com spring");
+			c.setCategory("front-end");
+
+			courseRepository.save(c);
+		};
+	}
 }
