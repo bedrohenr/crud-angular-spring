7a314d7a049b1193f553772f5ddf0f71ac1f96e3
Pedro Ferreira <ferfonhp@gmail.com>
2025-03-10 17:24:40 -0300

    retirando import não usado

--- crud-spring/src/main/java/com/ferfonhp/service/CourseService.java
+++ crud-spring/src/main/java/com/ferfonhp/service/CourseService.java
@@ -1,7 +1,6 @@
 package com.ferfonhp.service;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
