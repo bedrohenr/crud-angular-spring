ffd1c78615e973d0a919b273ec94236c321e883a
Pedro Ferreira <ferfonhp@gmail.com>
2025-03-12 20:07:25 -0300

    transicionando para usar o DTO no controller e DTO e mapper no service

--- crud-spring/src/main/java/com/ferfonhp/service/CourseService.java
+++ crud-spring/src/main/java/com/ferfonhp/service/CourseService.java
@@ -1,13 +1,15 @@
 package com.ferfonhp.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.ferfonhp.dto.CourseDTO;
+import com.ferfonhp.dto.mapper.CourseMapper;
 import com.ferfonhp.exception.RecordNotFoundException;
-import com.ferfonhp.model.Course;
 import com.ferfonhp.repository.CourseRepository;
 
 import jakarta.validation.Valid;
@@ -19,33 +21,50 @@ import jakarta.validation.constraints.Positive;
 public class CourseService {
     
     private final CourseRepository courseRepository;
+    private final CourseMapper courseMapper;
 
-    public CourseService(CourseRepository courseRepository) {
-        this.courseRepository = courseRepository;
+    public CourseService(CourseRepository courseRepository, CourseMapper courseMapper) {
+            this.courseRepository = courseRepository;
+            this.courseMapper = courseMapper;
     }
 
-    public List<Course> list() {
-        return courseRepository.findAll();
+    public List<CourseDTO> list() {
+        /* Forma mais básica */
+        // List<Course> courses = courseRepository.findAll();
+        // List<CourseDTO> dtos = new ArrayList<>(courses.size());
+        // for(Course course : courses){
+        //     CourseDTO dto = new CourseDTO(course.getId(), course.getName(), course.getCategory());
+        //     dtos.add(dto);
+        // }
+        // return courseRepository.findAll();
+        
+        // Faz o mapeamento Course para CourseDTO
+        return courseRepository.findAll()
+            .stream()
+            .map(course -> courseMapper.toDTO(course))
+            .collect(Collectors.toList());
     }
 
-    public Course findById(@PathVariable @NotNull @Positive Long id) {
-        return courseRepository.findById(id).orElseThrow(() -> new RecordNotFoundException(id));
+    public CourseDTO findById(@PathVariable @NotNull @Positive Long id) {
+        return courseRepository.findById(id)
+            .map(course -> courseMapper.toDTO(course))
+            .orElseThrow(() -> new RecordNotFoundException(id));
     }
 
-    public Course create(@Valid Course course) {
-        return courseRepository.save(course);
+    public CourseDTO create(@Valid @NotNull CourseDTO course) {
+        return courseMapper.toDTO(courseRepository.save(courseMapper.toEntity(course)));
     }
 
-    public Course update(
+    public CourseDTO update(
         @NotNull @Positive Long id,
-        @Valid Course course
+        @Valid @NotNull CourseDTO course
         ) {
         return courseRepository.findById(id)
             .map(recordFound -> {
-                recordFound.setName(course.getName());
-                recordFound.setCategory(course.getCategory());
+                recordFound.setName(course.name());
+                recordFound.setCategory(course.category());
 
-                return courseRepository.save(recordFound);
+                return courseMapper.toDTO(courseRepository.save(recordFound));
             }).orElseThrow(() -> new RecordNotFoundException(id));
     }
 
